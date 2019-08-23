6b4e2224b09f07a32bfcba0b44cbf8396a95b82e
Rachele Suy <rachelesuy@gmail.com>
2019-08-23 17:40:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,43 @@
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  
+  get 'recipes/new' do
+    erb :new
+  
+  end
+  # get '/recipes' do
+  #   @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[cook_time])
+  #   redirect "/recipes/#{@recipe.id}"
+  # end
+  
+    get '/recipes' do #loads index page
+    @recipes = Recipe.all
     erb :index
+  end
+  
+  post 'recipes'do
+    
+  end
+  
+  get 'recipes/id'  do
+    @recipe = Recipe..find_by_id(params[:id])
+    erb :show
+  end
+
+  patch '/recipes/:id' do  #updates a recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+   get '/recipes/:id/edit' do #loads edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
   end
 end
